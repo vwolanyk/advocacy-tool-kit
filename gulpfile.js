0ae279dc06306fb36b61833cb77d811d1ae4f475
Cassidy Kobewka <hello@cassidy.codes>
2018-06-24 13:41:51 -0400

    fixing gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -165,7 +165,7 @@ gulp.task('wiredep', () => {
     }))
     .pipe(gulp.dest('app/styles'));
 
-  gulp.src('app/**/*.html')
+  gulp.src('app/*.html')
     .pipe(wiredep({
       exclude: ['bootstrap'],
       ignorePath: /^(\.\.\/)*\.\./
